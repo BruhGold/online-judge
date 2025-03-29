780f64b331face2d1f16faa16ee2bda3669a5603
edamleanh <edamleanh@gmail.com>
2025-03-29 20:18:20 +0700

    Message

--- judge/views/api/api_v2.py
+++ judge/views/api/api_v2.py
@@ -32,24 +32,6 @@ from django.test import TestCase
 
 
 
-class LoginAPIView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            refresh = RefreshToken.for_user(user)  # Generate refresh token
-            return Response({
-                'message': 'Login successful',
-                'user_id': user.id,
-                'access_token': str(refresh.access_token),  # Include access token in response
-                'refresh_token': str(refresh)  # Include refresh token in response
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class BaseSimpleFilter:
     def __init__(self, lookup):
