841795fc4cbf6ac3cbd8dcbdee3e92b81b1cda43
BruhGold <nhdinh21@apcs.fitus.edu.vn>
2025-04-08 09:57:01 +0000

    update

--- judge/views/api/custom_apis/problem_data.py
+++ judge/views/api/custom_apis/problem_data.py
@@ -9,18 +9,14 @@ from judge.models.problem_data import ProblemData
 from ..permissions.problem import CanEditProblem
 from ..serializers.problem_data import ProblemDataSerializer
 from judge.utils.problem_data import ProblemDataCompiler, ProblemDataError
+from judge.views.problem_data import ProblemManagerMixin
 
-class APIProblemDataView(APIView):
+class APIProblemDataView(APIView, ProblemManagerMixin):
     permission_classes = [IsAuthenticated,CanEditProblem]
     parser_classes = [MultiPartParser]
 
-    def get_object(self, problem_code):
-        problem = get_object_or_404(Problem, code=problem_code)
-        self.check_object_permissions(self.request, problem)
-        return problem
-
-    def get(self, request, problem, *args, **kwargs):
-        problem_obj = self.get_object(problem)
+    def get(self, request, *args, **kwargs):
+        problem_obj = self.get_object()
         try:
             problem_data = problem_obj.data_files
         except ProblemData.DoesNotExist:
@@ -29,17 +25,7 @@ class APIProblemDataView(APIView):
         serializer = ProblemDataSerializer(problem_data)
         return Response(serializer.data)
 
-    def put(self, request, problem, *args, **kwargs):
-        problem_obj = self.get_object(problem)
+    def put(self, request, *args, **kwargs):
+        problem_obj = self.get_object()
         self.check_object_permissions(request, problem_obj)
-
-        try:
-            problem_data = problem_obj.data_files
-        except ProblemData.DoesNotExist:
-            return Response({"detail": "ProblemData not found."}, status=status.HTTP_404_NOT_FOUND)
-        
-        serializer = ProblemDataSerializer(problem_data, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return super().put(request, problem_obj, *args, **kwargs)
