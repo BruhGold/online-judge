a5945c2ac11aea7e1fa2ed556b5800399b4a9986
BruhGold <nhdinh21@apcs.fitus.edu.vn>
2025-04-28 12:01:18 +0000

    Update Problem Data ZipFile

--- dmoj/urls.py
+++ dmoj/urls.py
@@ -119,7 +119,7 @@ urlpatterns = [
         path('/test_data', ProblemDataView.as_view(), name='problem_data'),
         path('/test_data/init', problem_init_view, name='problem_data_init'),
         path('/test_data/diff', ProblemSubmissionDiff.as_view(), name='problem_submission_diff'),
-        path('/data/<path:path>', problem_data_file, name='problem_data_file'),
+        path('/data/<path:path>', problem_data_file.as_view(), name='problem_data_file'),
 
         path('/tickets', ticket.ProblemTicketListView.as_view(), name='problem_ticket_list'),
         path('/tickets/new', ticket.NewProblemTicketView.as_view(), name='new_problem_ticket'),
